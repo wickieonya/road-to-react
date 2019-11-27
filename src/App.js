e5dd5e9e7e8746f93c1d5f5800048733eed59c7e
wickieonya <wickieonya@gmail.com>
2019-11-27 13:31:57 +0300

    added hackernews api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-  {
-    title: "Flask",
-    url: "https://pocco.palletes.flask.org",
-    author: "A B",
-    num_comments: 3,
-    points: 41,
-    objectID: 2
-  }
-];
+const DEFAULT_QUERY = "flask";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
 
 const isSearched = search_term => item =>
   item.title.toLocaleLowerCase().includes(search_term.toLocaleLowerCase());
@@ -35,9 +14,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      search_term: ""
+      result: null,
+      search_term: DEFAULT_QUERY
     };
+  }
+
+  setSearchTopStories = result => {
+    this.setState({ result });
+  };
+
+  componentDidMount() {
+    const { search_term } = this.state;
+    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${search_term}`;
+    axios.get(url)
+    .then( response => this.setSearchTopStories(response.data.hits))
+    .catch( error => console.log(error));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.result);
   }
 
   onDismiss = id => {
@@ -51,7 +46,10 @@ class App extends Component {
   };
 
   render() {
-    const { search_term, list } = this.state;
+    const { search_term, result } = this.state;
+    if (!result) { return null; }
+    console.log(result);
+
     return (
       <div className="page">
         <div className="interactions">
@@ -59,7 +57,7 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table list={list} pattern={search_term} onDismiss={this.onDismiss} />
+        <Table list={result} pattern={search_term} onDismiss={this.onDismiss} />
       </div>
     );
   }
@@ -86,10 +84,10 @@ const Table = ({ list, pattern, onDismiss }) => (
           <span style={{ width: "40%" }}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span style={{ width: '30%'}}> {item.author} </span>
-          <span style={{ width: '7%'}}> {item.num_comments} </span>
-          <span style={{ width: '7%'}}> {item.points} </span>
-          <span style={{ width: '16%'}}>
+          <span style={{ width: "30%" }}> {item.author} </span>
+          <span style={{ width: "7%" }}> {item.num_comments} </span>
+          <span style={{ width: "7%" }}> {item.points} </span>
+          <span style={{ width: "16%" }}>
             <Button
               onClick={() => onDismiss(item.objectID)}
               className="button-inline"
